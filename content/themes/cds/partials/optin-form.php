1b87e2b8f8fe975f8f4eeb5a3337a71e0e22017f
Sam White <sam.cdsg@gmail.com>
2014-07-24 16:25:07 -0500

    Opt In Landing page
    Added radio buttons and changed the Thank you text

--- content/themes/cds/partials/optin-form.php
+++ content/themes/cds/partials/optin-form.php
@@ -9,10 +9,10 @@
 <form class="optin" action="https://s1851.t.eloqua.com/e/f2" method="post" target="eloqua-submit" name="optinform" id="optinform">
 <p>Sign up for our email list below. We'll keep you up to date on the categories of your choice.</p><label class="hidefromscreen" for="emailAddress">Email Address:</label><input id="optin_email" name="emailAddress" required="" type="email" placeholder="Email Address" /><br><br>
 <label class="checkbox" for="industry"><strong>Industry categories you are interested in:</strong><br>
-<input name="industry" type="checkbox" value="Media" /> Media<br>
-<input name="industry" type="checkbox" value="Not For Profit" /> Nonprofit<br>
-<input name="industry" type="checkbox" value="Utilities" /> Utilities<br>
-<input name="industry" type="checkbox" value="Other" /> Other</label><br>
+<input name="industry" type="radio" value="Media" /> Media<br>
+<input name="industry" type="radio" value="Not For Profit" /> Nonprofit<br>
+<input name="industry" type="radio" value="Utilities" /> Utilities<br>
+<input name="industry" type="radio" value="Other" /> Other</label><br>
 <hr class="fadeline" />
 <label class="checkbox" for="legal"><input name="OptInCheckbox" type="checkbox" value="yes" required="true" /> By checking this box, I authorize CDS Global to contact me via the email address supplied about CDS Global products and services, including product releases, updates, seminars, events, surveys, trainings and special offers.</label>
 <input type="hidden" name="PermissionDate" value="<?php echo date("m/d/Y"); ?>" />
@@ -21,7 +21,7 @@
 <hr class="fadeline" />
 <div class="form-actions">
 
-<button class="btn btn-danger optin_submit" id="optinSubmit" type="submit">STAY INFORMED</button></div>
+<button class="btn btn-danger optin_submit" id="optinSubmit" type="submit">STAY INFORMED!</button></div>
 </form>
 <p class="optin_note">All messages are moderated and always on topic. You can unsubscribe at any time.</p>
 </div>
@@ -32,13 +32,14 @@
 <div id="thankyou" style="display: none;">
 <div class="row">
 <div class="col-xs-10 col-xs-offset-1">
-Thank you!
+<h3 style="color: #fc4c00;">Thank you for signing up to stay up to date on our latest products and services.</h3>
+<p class="optin_note">All messages are moderated and always on topic. You can unsubscribe at any time.</p>
 </div>
 </div>
 </div>
 <!-- End Thank you text -->
 </div>
-<iframe name="eloqua-submit" style="display: block"></iframe>
+<iframe name="eloqua-submit" style="display: none;"></iframe>
 <script>
 $('#optinform').submit(function(e) {
     e.preventDefault(); // don't submit multiple times
