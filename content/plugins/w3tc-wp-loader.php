303b39a62ac628398dc227911c0afc68d9421335
Sam White <sam.cdsg@gmail.com>
2014-08-12 17:04:22 -0500

    new plugins
    three wp broadcaster

--- content/plugins/w3tc-wp-loader.php
+++ content/plugins/w3tc-wp-loader.php
@@ -1,4 +1,4 @@
 
 <?php
     if (W3TC_WP_LOADING)
-        require_once 'C:/www/cds_global/wordpress/wp-load.php';
+        require_once '/Users/SamWhite/www/sites/cds_global/wordpress/wp-load.php';
