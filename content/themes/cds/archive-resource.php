6f576f3651f43f37b9b61ec91c40bf83c5f90650
Sam White <sam.cdsg@gmail.com>
2014-09-12 10:02:08 -0500

    fixed resources from wp 4.0 update

--- content/themes/cds/archive-resource.php
+++ content/themes/cds/archive-resource.php
@@ -86,16 +86,38 @@ setup_postdata($post);
             <?php
                 if (have_posts()): ?>
                 <ul class="listing resources">
+
                         <?php
-                        global $wp_query;
-                        $args = array_merge( $wp_query->query_vars, array( 'meta_key' => 'resource_priority', 'orderby' => 'meta_value date', 'order' => 'ASC DESC') );
+
+                        if (isset($_GET['industry']) && isset($_GET['type'])) {
+                            $query = new WP_Query( array( 'post_type' => 'resource', 'meta_query' => array(	array('key' => 'industry', 'value'   => $_GET['industry'], 'compare' => 'LIKE', ),), 'tax_query' => array( array( 'taxonomy' => 'resource_type', 'field' => 'slug', 'terms' => $_GET['type'], ), ), 'orderby' => array( 'meta_value_num' => 'ASC', 'date' => 'DESC' ), 'meta_key' => 'resource_priority' ) );
+                        } elseif (isset($_GET['industry'])) {
+                            $query = new WP_Query( array( 'post_type' => 'resource', 'meta_query' => array(	array('key' => 'industry', 'value'   => $_GET['industry'], 'compare' => 'LIKE', ),), 'orderby' => array( 'meta_value_num' => 'ASC', 'date' => 'DESC' ), 'meta_key' => 'resource_priority' ) );
+                        } elseif (isset($_GET['type'])) {
+                            $query = new WP_Query( array( 'post_type' => 'resource', 'tax_query' => array( array( 'taxonomy' => 'resource_type', 'field' => 'slug', 'terms' => $_GET['type'], ), ), 'orderby' => array( 'meta_value_num' => 'ASC', 'date' => 'DESC' ), 'meta_key' => 'resource_priority' ) );
+                        } else {
+                            $query = new WP_Query( array( 'post_type' => 'resource', 'orderby' => array( 'meta_value_num' => 'ASC', 'date' => 'DESC' ), 'meta_key' => 'resource_priority' ) );
+                        }
+
+                        while ( $query->have_posts() ): $query->the_post();
+                            get_template_part('partials/resource', get_post_format());
+                        endwhile;
+
+                        /*while (have_posts()): the_post();
+                           get_template_part('partials/resource', get_post_format());
+                        endwhile;*/
+
+                        /*global $wp_query;
+                        //$args = array_merge( $wp_query->query_vars, array( 'meta_key' => 'resource_priority', 'orderby' => 'meta_value date', 'order' => 'ASC DESC') );
+                        $query = new WP_Query( array( 'post_type' => 'news', 'orderby' => array( 'meta_value_num' => 'ASC', 'date' => 'DESC' ), 'meta_key' => 'resource_priority' ) );
+                        //$query = new WP_Query( array ( 'post_type' => 'resource', 'orderby' => 'date', 'order' => 'ASC' ) );
                         query_posts( $args );
 
                         while (have_posts()): the_post();
                             get_template_part('partials/resource', get_post_format());
-                        endwhile;
+                        endwhile;*/
 
-                        wp_reset_query(); ?>
+                        //wp_reset_query(); ?>
                 </ul>
                 <nav class="pagination">
                     <div class="previous"><?php previous_posts_link('Previous page'); ?></div>
