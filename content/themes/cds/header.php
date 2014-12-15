ad6d58afe8e88822c25f8bf27065f9247c8e1c5a
Sam White <sam.cdsg@gmail.com>
2014-12-15 10:19:04 -0600

    Updated GA code in header

--- content/themes/cds/header.php
+++ content/themes/cds/header.php
@@ -47,7 +47,18 @@
     <script src="//cdn.optimizely.com/js/2211952690.js"></script>
 </head>
 <body <?php body_class(); ?>>
-    <?php if ($cdsdomain == "www.cds-global.com" || $cdsdomain == "stage.cds-global.com") { ?>
+    <?php if ($cdsdomain == "stage.cds-global.com") { ?>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-668234-13', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <?php } else { ?>
     <!-- Google Tag Manager -->
         <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N5XJNT"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
@@ -57,16 +68,6 @@
         '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-N5XJNT');</script>
     <!-- End Google Tag Manager -->
-    <?php } elseif ($cdsdomain == "www.cdsglobal.co.uk") { ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-668234-4', 'auto');
-      ga('send', 'pageview');
-
     </script>
     <?php } ?>
     <nav role="navigation">
