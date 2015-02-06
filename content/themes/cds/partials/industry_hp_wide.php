eff71417cc7e13909eb975f2d7b156f6c8fb968a
Sam White <sam.cdsg@gmail.com>
2015-02-06 15:41:04 -0600

    Added Higher Ed to homepage industry section

--- content/themes/cds/partials/industry_hp_wide.php
+++ content/themes/cds/partials/industry_hp_wide.php
@@ -4,13 +4,8 @@ $cdsdomain = $_SERVER['SERVER_NAME'];
     ?>
         <ul>
             <li>
-                <div><a href="/industries/consumer-products/"><img width="85" height="80" src="/content/uploads/2014/12/consumer-products.png" class="attachment-post-thumbnail" alt="consumer-products" /></a><br />
-                <a href="/industries/consumer-products/" style="color: #8dc63f;">Consumer Products</a></div>
-                <p>Convert visitors into customers and customers into brand advocates with effective, turnkey, multichannel eCommerce solutions that drive results and revenues.</p>
-            </li>
-            <li>
-                <div><a href="/industries/media/"><img width="85" height="80" src="/content/uploads/2014/12/media.png" class="attachment-post-thumbnail" alt="media" /></a><br />
-                <a href="/industries/media/" style="color:#4e4a42;">Media</a></div>
+                <div><a href="/industries/media/"><img width="85" height="80" src="/content/uploads/2015/02/media_icon_color.png" class="attachment-post-thumbnail" alt="media" /></a><br />
+                <a href="/industries/media/" style="color:#00b5e5;">Media</a></div>
                 <p>Be a leader in the ever-changing print and digital landscape with innovative, sustainable and highly cost-effective solutions to attract, retain and interact with consumers.</p>
             </li>
             <li>
@@ -19,9 +14,14 @@ $cdsdomain = $_SERVER['SERVER_NAME'];
                 <p>Simplify the donation process and cultivate lasting donor relationships with innovative data, mailing, and caging solutions that power the modern nonprofit.</p>
             </li>
             <li>
-                <div><a href="/industries/utilities/"><img width="85" height="80" src="/content/uploads/2014/12/utilities.png" class="attachment-post-thumbnail" alt="utilities" /></a><br />
-                <a href="/industries/utilities/" style="color:#00b5e5;">Utilities</a></div>
-                <p>Improve customer service, decrease operating costs and boost profitability with solutions designed to meet utilities’ specific payment processing and communication needs.</p>
+                <div><a href="/industries/higher-education/"><img width="85" height="80" src="/content/uploads/2015/02/higher_ed_icon_color.png" class="attachment-post-thumbnail" alt="nonprofit" /></a><br />
+                <a href="/industries/higher-education/" style="color:#4e4a42;">Higher Education</a></div>
+                <p>Shift your focus from back-end operations to creating meaningful interactions with alumni, prospective donors and other supporters.</p>
+            </li>
+            <li>
+                <div><a href="/industries/consumer-products/"><img width="85" height="80" src="/content/uploads/2014/12/consumer-products.png" class="attachment-post-thumbnail" alt="consumer-products" /></a><br />
+                <a href="/industries/consumer-products/" style="color: #8dc63f;">Consumer Products</a></div>
+                <p>Convert visitors into customers and customers into brand advocates with effective, turnkey, multichannel eCommerce solutions that drive results and revenues.</p>
             </li>
         </ul>
         <?php } elseif ($cdsdomain == "www.cdsglobal.co.uk" || $cdsdomain == "uk.cds-global.com" || $cdsdomain == "stageuk.cds-global.com") { ?>
